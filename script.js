42fe8b21d4d8076a0d3fa025606851590817e355
sticksalt-ux <nathanisfun200@gmail.com>
2023-03-12 18:41:15 -0700

    just a test project for an image gallery

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll('.panel');
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeClass();
+        panel.classList.add('active')
+    })
+})
+
+function removeClass() {
+    panels.forEach((panel) => {
+        panel.classList.remove('active')
+    })
+}
